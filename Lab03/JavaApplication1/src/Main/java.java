8f88495d08377554e6c23d69287d2a05f19dc270
Jakubrutkowski <jakubrutkowski95@gmail.com>
2020-03-29 23:00:17 +0200

    Testowy commit

--- Lab03/JavaApplication1/src/Main/java.java
+++ Lab03/JavaApplication1/src/Main/java.java
@@ -1,21 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
 
-/**
- *
- * @author komp
- */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 public class java {
 
-    /**
-     * @param args the command line arguments
-     */
+
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        
+        List<String> nazwiska = new ArrayList<>();
+        nazwiska.add("Kowalski");
+        nazwiska.add("Nowak");
+        
+        for(String nazwisko : nazwiska){
+            System.out.println(nazwisko);
+        }
+        
+        for (int i=0; i<nazwiska.size(); i++){
+            String nazwisko = nazwiska.get(i);
+            System.out.println(nazwisko);
+        }
+        
+        
+        Map<String, String> dniTygodnia = new HashMap<>();
+        dniTygodnia.put("pn", "poniedziałek");
+        dniTygodnia.put("wt", "wtorek");
+        dniTygodnia.put("sr", "sroda");
+        dniTygodnia.put("czw", "czwartek");
+        
+        String dzien1 = dniTygodnia.get("pn");
+        System.out.println("Skrot 'pn' oznacza: " + dzien1);
+        
+        for (String skrot : dniTygodnia.keySet()){
+            String dzien = dniTygodnia.get(skrot);
+            System.out.println(skrot + " - " + dzien);
+        }
+        
+        
+     
+        
     }
     
 }
