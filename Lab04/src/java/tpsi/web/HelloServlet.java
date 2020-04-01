79a125c42ce305ec6faa4698f08ed630be2b256a
Jakubrutkowski <jakubrutkowski95@gmail.com>
2020-04-01 21:55:21 +0200

    Przekierowanie z serwletu do JSP

--- Lab04/src/java/tpsi/web/HelloServlet.java
+++ Lab04/src/java/tpsi/web/HelloServlet.java
@@ -14,20 +14,9 @@ public class HelloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        response.setContentType("text/html;charset=UTF-8");
-
-        try (PrintWriter out = response.getWriter()) {
             String imie = request.getParameter("imie");
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Java Web - laboratorium 4</title>");
-            out.println("</head>");
-            out.println("<body>");
-//            out.println("<h1>Servlet mówi Hello</h1>");
-            out.println("<h1>Cześć, " + imie + "!</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+            request.setAttribute("imie", imie);
+            request.getRequestDispatcher("hello.jsp").forward(request, response);
+
     }
 }
